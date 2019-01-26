7884df0fe019495d40f9db5f15216a5385187bef
panJJ <panjakubjakubowski@gmail.com>
2019-01-26 23:58:53 +0000

    Main screen setup

---
+++ Budomax/Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+    public float speed;
+    public float range;
+    public bool bounce;
+    public bool startLeft;
+    private Vector3 direction;
+    private Vector3 startPos;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startLeft) direction = new Vector3(-speed, 0, 0);
+        else direction = new Vector3(speed, 0, 0);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.Distance(startPos, transform.position).ToString());
+        transform.Translate(direction);
+        if (bounce)
+        {
+            distance += speed;
+            if ( distance >= range)
+            {
+                direction = direction *-1;
+                distance = 0;
+          
+            }
+               
+        }
+
+    }
+}
