0e8953dded62335d5dafe195e956818e0e5d2717
Michał Roźniakowski <rozniakowski.michal@gmail.com>
2019-01-26 10:48:32 +0100

    Raycast based rooms

---
+++ Budomax/Assets/Scripts/Raycast Based Rooms/Wall.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour, IDamageable
+{
+    public StructureConfig wallConfig;
+
+   
+    public List<Wall> neighbours = new List<Wall>();
+    Rigidbody2D wallRb;
+    Collider2D wallCollider;
+    float currentHP;
+
+
+
+    void Awake()
+    {
+        // Get Components
+        wallRb = GetComponent<Rigidbody2D>();
+        wallCollider = GetComponent<Collider2D>();
+
+        // Process config info
+        wallRb.gravityScale = wallConfig.structureGravityMultiplier;
+        wallRb.bodyType = wallConfig.defaultRBType;
+        wallCollider.isTrigger = wallConfig.triggerByDefault;
+        currentHP = wallConfig.structureHP;
+    }  
+
+
+
+    public void DropWall()
+    {
+        wallCollider.isTrigger = true;
+        wallRb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    public void PlaceWall(List<GameObject> neighbourWalls)
+    {
+        for (int nId = 0; nId < neighbourWalls.Count; nId++)
+        {
+            Wall tempWall = neighbourWalls[nId].GetComponentInParent<Wall>();
+            neighbours.Add(tempWall);
+            tempWall.neighbours.Add(this);
+        }
+
+        wallCollider.isTrigger = false;
+        wallRb.bodyType = RigidbodyType2D.Static;
+
+        GetComponent<WallRoomChecker>()?.CheckForPossibleRooms();
+    }
+
+
+
+    public void TakeDamage(float damage)
+    {
+        if (wallConfig.damageable)
+        {
+            Debug.Log("Wall damaged");
+            currentHP -= damage;
+
+            if (currentHP < 0f)
+                DropWall();
+        }
+    }
+}
---
+++ Budomax/Assets/Scripts/Raycast Based Rooms/WallPlacer.cs
@@ -0,0 +1,85 @@
+﻿// Main home does not have this script attached
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPlacer : MonoBehaviour
+{
+    public float rotationSpeed = 200f;
+
+    List<GameObject> attachablesInRange = new List<GameObject>();
+    List<GameObject> restictedAreasInRange = new List<GameObject>();
+
+    WallInteractionMode interactionMode = WallInteractionMode.Targetable;
+    Vector3 mouseOffset;
+
+
+    void LateUpdate()
+    {
+        ProcessRotation();
+    }
+
+    void ProcessRotation()
+    {
+        if (interactionMode == WallInteractionMode.Draggable)
+        {
+            if (Input.GetKey(KeyCode.Q))
+                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime); // Rotate left
+            else if (Input.GetKey(KeyCode.E))
+                transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime); // Rotate right
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        if (interactionMode == WallInteractionMode.Targetable)
+        {
+            interactionMode = WallInteractionMode.Draggable;
+            mouseOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+        }
+    }
+
+    public void OnMouseDrag()
+    {
+        if (interactionMode == WallInteractionMode.Draggable)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + mouseOffset;
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        if (interactionMode == WallInteractionMode.Draggable)
+        {
+            interactionMode = WallInteractionMode.NotTargetable;
+            
+            if (CanBePlacedHere()) GetComponent<Wall>()?.PlaceWall(attachablesInRange);
+            else GetComponent<Wall>()?.DropWall();
+        }
+    }
+
+
+
+    bool CanBePlacedHere()
+    {
+        return (3 > attachablesInRange.Count && attachablesInRange.Count > 0) && restictedAreasInRange.Count == 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("AttachableArea") && !attachablesInRange.Contains(collision.gameObject)) attachablesInRange.Add(collision.gameObject);
+        if (collision.CompareTag("NotAttachableArea") && !restictedAreasInRange.Contains(collision.gameObject)) restictedAreasInRange.Add(collision.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("AttachableArea") && attachablesInRange.Contains(collision.gameObject)) attachablesInRange.Remove(collision.gameObject);
+        if (collision.CompareTag("NotAttachableArea") && restictedAreasInRange.Contains(collision.gameObject)) restictedAreasInRange.Remove(collision.gameObject);
+    }
+
+    enum WallInteractionMode
+    {
+        Targetable,
+        Draggable,
+        NotTargetable,
+    }
+}
