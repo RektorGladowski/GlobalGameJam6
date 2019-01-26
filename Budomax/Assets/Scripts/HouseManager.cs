ad11d1efb07984d3973b2593970a30c0f75366ef
chrustol <chrustol@gmail.com>
2019-01-26 01:23:31 +0100

    Messing with Room

--- Budomax/Assets/Scripts/HouseManager.cs
+++ Budomax/Assets/Scripts/HouseManager.cs
@@ -1,26 +1,27 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using PolygonArea;
+using System;
 
 [RequireComponent(typeof(CompositeCollider2D))]
 public class HouseManager : MonoBehaviour
 {
+    public Action<RoomData[]> OnHouseRebuild;
+
     // Read all the colliders and create a combined Polygon2D collider from the others. See PolygonCollider2D in the docs, specifically pathCount and SetPath. 
     public CompositeCollider2D compositeCollider { get { if (_compositeCollider == null) _compositeCollider = GetComponent<CompositeCollider2D>(); return _compositeCollider; } }
     private CompositeCollider2D _compositeCollider;
 
     private RoomData outerWalls;
-
-
     private List<RoomData> roomDatas = new List<RoomData>();
 
-    // Rather unnecessary ;)
     private void Awake() { Rebuild(); }
 
     public void Rebuild()
     {
+        roomDatas.Clear();
         outerWalls = new RoomData(new Vector2[] { new Vector2(0, 0), new Vector2(0, 0) });
-
+        
         for (int i = 0; i < compositeCollider.pathCount; i++)
         {
             Vector2[] points = new Vector2[compositeCollider.GetPathPointCount(i)];
@@ -31,6 +32,7 @@ public class HouseManager : MonoBehaviour
             if (outerWalls.Area < roomData.Area) outerWalls = roomData;
             else roomDatas.Add(roomData);
         }
+        OnHouseRebuild(roomDatas.ToArray());
     }
 
     public bool IsTouching(Collider2D collider2d)
@@ -50,11 +52,13 @@ public class RoomData
 {
     public Vector2[] Points { get; private set; }
     public float Area { get; private set; }
+    public Mesh Mesh { get; private set; }
 
     public RoomData(Vector2[] points)
     {
         Points = points;
         Area = points.Area();
+        Mesh = MeshGenerator.GenerateMesh(Points);
     }
 
 }
---
+++ Budomax/Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HouseManager))]
+public class RoomGenerator : MonoBehaviour
+{
+    public HouseManager houseManager { get { if (_houseManager == null) _houseManager = GetComponent<HouseManager>(); return _houseManager; } }
+    private HouseManager _houseManager;
+
+    [SerializeField] private Material kitchenMaterial;
+    [SerializeField] private Material baracksRoomMaterial;
+    [SerializeField] private Material scavengerRoomMaterial;
+
+    private void Awake() { houseManager.OnHouseRebuild = UpdateRooms; }
+    private void OnDestroy() { houseManager.OnHouseRebuild = null; }
+
+    private void UpdateRooms(RoomData[] roomDatas)
+    {
+        for (int i = 0; i < roomDatas.Length; i++)
+        {
+            GameObject go = new GameObject("RoomName", typeof(MeshFilter), typeof(MeshRenderer));
+            go.GetComponent<MeshFilter>().mesh = roomDatas[i].Mesh;
+        }
+    }
+
+
+}
