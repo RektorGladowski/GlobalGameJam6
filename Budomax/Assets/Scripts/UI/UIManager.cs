e444c7eb6f489d9f4df129f7ec881aa46af2a4df
Michał Roźniakowski <rozniakowski.michal@gmail.com>
2019-01-27 04:46:47 +0100

    UI counters

--- Budomax/Assets/Scripts/UI/UIManager.cs
+++ Budomax/Assets/Scripts/UI/UIManager.cs
@@ -1,42 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
 
+    public UIHeightCounter heightCounter;
+    public UIEnergyCounter energyCounter;
+    public UIUnitCounter cooksCounter, scavsCounter, warriorsCounter;
+
+    #region Awake and Updates
     void Awake() => instance = this;
 
-    #region Public for updating UI
-    public void UpdateTotalHeight (float height)
+    void LateUpdate()
     {
-
+        heightCounter.UpdateCurrentValue(Time.deltaTime);
+        energyCounter.UpdateCurrentValue(Time.deltaTime);
+        cooksCounter.UpdateCurrentValue(Time.deltaTime);
+        scavsCounter.UpdateCurrentValue(Time.deltaTime);
+        warriorsCounter.UpdateCurrentValue(Time.deltaTime);
     }
+    #endregion
 
-    public void UpdateAverageEnergy (float energyPercent)
+    #region Public methods for updating UI
+    public void UpdateTotalHeight(float height) => heightCounter.SetTargetValue(height);
+
+    public void UpdateAverageEnergy(float energyPercent) => energyCounter.SetTargetValue(energyPercent);
+
+    public void UpdateCooksNumber(int cooks) => cooksCounter.SetTargetValue(cooks);
+    public void UpdateScavsNumber(int scavs) => scavsCounter.SetTargetValue(scavs);
+    public void UpdateWarriorsNumber(int warriors) => warriorsCounter.SetTargetValue(warriors);
+
+    public void UpdateUnitsNumbers(int cooks, int scavs, int warriors)
     {
-
-    }
-
-    public void UpdateCooksNumber (int cooks)
-    {
-
-    }
-
-    public void UpdateScavsNumber (int scavs)
-    {
-
-    }
-
-    public void UpdateWarriorsNumber (int warriors)
-    {
-
-    }
-
-    public void UpdateUnitsNumbers (int cooks, int scavs, int warriors)
-    {
-
+        cooksCounter.SetTargetValue(cooks);
+        scavsCounter.SetTargetValue(scavs);
+        warriorsCounter.SetTargetValue(warriors);
     }
     #endregion
 }
